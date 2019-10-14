49830737fe54c5ff4e8b5ff7aa2166302068a34d
lainz <leandrodiazoe@gmail.com>
2019-10-14 11:22:42 -0300

    Generated links

--- app.js
+++ app.js
@@ -6,4 +6,18 @@
     if (navigator.appVersion.indexOf("Linux") != -1) OSName = "Linux";
 
     $('#os-hint').text('Your current Operating System is ' + OSName);
+
+    var repo = 'https://github.com/bgrabitmap/lazpaint/releases/download/'
+    var version = '7.0.7'
+
+    document.getElementById('title').innerHTML = `Download LazPaint ${version}`
+    document.getElementById('link_win32_no_install').setAttribute('href', `${repo}v${version}/lazpaint${version}_win32_no_install.zip`)
+    document.getElementById('link_win64_no_install').setAttribute('href', `${repo}v${version}/lazpaint${version}_win64_no_install.zip`)
+    document.getElementById('link_installer_win32_win64').setAttribute('href', `${repo}v${version}/lazpaint${version}_setup_win32_win64.exe`)
+    document.getElementById('link_linux32_no_install').setAttribute('href', `${repo}v${version}/lazpaint${version}_linux32_no_install.tar.gz`)
+    document.getElementById('link_linux64_no_install').setAttribute('href', `${repo}v${version}/lazpaint${version}_linux64_no_install.tar.gz`)
+    document.getElementById('link_linux32_deb').setAttribute('href', `${repo}v${version}/lazpaint${version}_linux32.deb`)
+    document.getElementById('link_linux64_deb').setAttribute('href', `${repo}v${version}/lazpaint${version}_linux64.deb`)
+    document.getElementById('link_macos32_dmg').setAttribute('href', `${repo}v${version}/lazpaint${version}_macos32.dmg`)
+    document.getElementById('link_macos64_dmg').setAttribute('href', `${repo}v${version}/lazpaint${version}_macos64.dmg`)
 })();
